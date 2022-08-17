509007a15c07e3cbc1f7a06f7f34b7e328271915
bonecas <filipabartlett@gmail.com>
2022-08-17 12:31:24 +0200

    -[O1
    2HK

---
+++ JS/emailValidation.js
@@ -0,0 +1,14 @@
+function validate() {
+    var input = document.createElement('input');
+    input.type='email';
+    input.value=document.getElementById('email').value;
+      
+    if (input.checkValidity()) {
+      document.getElementById('result').innerText = 'Email has been sent';
+    } else {
+      document.getElementById('result').innerText = 'Please enter a valid email';
+      document.getElementById('result').style.color = 'red';
+    }
+      
+    return false;
+  }
